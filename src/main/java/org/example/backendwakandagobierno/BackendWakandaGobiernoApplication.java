916f0cd25fe7009a598656e0cf54f63afaf39308
marcos <marcgabe@myuax.com>
2024-11-26 13:06:03 +0100

    Arquitectura V1.0
    Inicial Commit

---
+++ src/main/java/org/example/backendwakandagobierno/BackendWakandaGobiernoApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandagobierno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaGobiernoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWakandaGobiernoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/backendwakandagobierno/BackendWakandaGobiernoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandagobierno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaGobiernoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
